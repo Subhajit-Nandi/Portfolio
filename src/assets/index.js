d3e719c69932f248bf1331f70fb09f79d895c6d1
Subhajit Nandi <subhajitnandi1343@gmail.com>
2023-08-30 02:10:50 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,66 @@
+import logo from "./logo.svg";
+import logo1 from "./logo1.png"
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import susanta from "./susanta.jpg";
+import samik from "./samik.jpg";
+import Sam from "./Sam.jpg";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import threejs from "./tech/threejs.svg";
+import Cplusplus from "./tech/Cplusplus.png";
+import bootstrap from "./tech/bootstrap.png";
+import python from "./tech/python.png";
+
+
+import BEE from "./company/BEE.png";
+
+
+import Todura from "./Todura.png";
+import CyberViON from "./CyberViON.png";
+import NewsBEE from "./NewsBEE.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  susanta,
+  samik,
+  Sam,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  threejs,
+  CyberViON,
+  Todura,
+  NewsBEE,
+  BEE,
+  logo1,
+  Cplusplus,
+  bootstrap,
+  python,
+};
---
+++ src/constants/index.js
@@ -0,0 +1,226 @@
+import {
+    mobile,
+    backend,
+    creator,
+    web,
+    javascript,
+    bootstrap,
+    html,
+    css,
+    reactjs,
+    susanta,
+    samik,
+    Sam,
+    redux,
+    tailwind,
+    nodejs,
+    mongodb,
+    git,
+    Cplusplus,
+    BEE,
+    Todura,
+    threejs,
+    python,
+    CyberViON,
+    NewsBEE,
+} from "../assets";
+
+const navLinks = [
+    {
+        id: "about",
+        title: "About",
+    },
+    {
+        id: "work",
+        title: "Work",
+    },
+    {
+        id: "contact",
+        title: "Contact",
+    },
+];
+
+const services = [
+    {
+        title: "Web Developer",
+        icon: web,
+    },
+    {
+        title: "Algorithm Architect",
+        icon: mobile,
+    },
+    {
+        title: "Backend Developer",
+        icon: backend,
+    },
+    {
+        title: "Content Creator",
+        icon: creator,
+    },
+];
+
+const technologies = [
+    {
+        name: "HTML 5",
+        icon: html,
+    },
+    {
+        name: "CSS 3",
+        icon: css,
+    },
+    {
+        name: "JavaScript",
+        icon: javascript,
+    },
+    {
+        name: "React JS",
+        icon: reactjs,
+    },
+    {
+        name: "Redux Toolkit",
+        icon: redux,
+    },
+    {
+        name: "Tailwind CSS",
+        icon: tailwind,
+    },
+    {
+        name: "Bootstrap",
+        icon: bootstrap,
+    },
+    {
+        name: "Node JS",
+        icon: nodejs,
+    },
+    {
+        name: "MongoDB",
+        icon: mongodb,
+    },
+    {
+        name: "Three JS",
+        icon: threejs,
+    },
+    {
+        name: "git",
+        icon: git,
+    },
+    {
+        name: "C/C++",
+        icon: Cplusplus,
+    },
+    {
+        name: "Python",
+        icon: python,
+    }
+];
+
+const experiences = [
+    {
+        title: "Full stack Developer",
+        company_name: "BEE",
+        icon: BEE,
+        iconBg: "#E6DEDD",
+        date: "Jan 2023 - Present",
+        points: [
+            "PRIME MOVER",
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+];
+
+const testimonials = [
+    {
+        testimonial:
+            "A good leader with a great vision, a great team player and a good mentor. He can be a great asset to any team.",
+        name: "Susanta Das",
+        designation: "CFO",
+        company: "BEE",
+        image: susanta,
+    },
+    {
+        testimonial:
+            "A fantastic professional who is always willing to go the extra mile to get the job done. I am endorsing him for his credibility and sheer determination to succeed.",
+        name: "Samik Pandit",
+        designation: "COO",
+        company: "CyberViON",
+        image: samik,
+    },
+    {
+        testimonial:
+            "I highly recommend Xavier as a candidate for employment. He is a team player and would make a great asset to any organization.",
+        name: "Sam",
+        designation: "SDE",
+        company: "BEE",
+        image: Sam,
+    },
+];
+
+const projects = [
+    {
+        name: "NewsBEE",
+        description:
+            "NewsBEE is a personalized news aggregator that organizes and highlights what's happening in the world so you can discover more about the stories that matter to you.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "newsapi",
+                color: "green-text-gradient",
+            },
+            {
+                name: "bootstrap",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: NewsBEE,
+        source_code_link: "https://github.com/Subhajit-Nandi/NewsBEE",
+    },
+    {
+        name: "TODURA",
+        description:
+            "Web-based platform that gives you a quick and simple notepad editing experience when you write notes, memo, email, message, shopping list and to do list.",
+        tags: [
+            {
+                name: "react",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "mongodb",
+                color: "green-text-gradient",
+            },
+            {
+                name: "bootstrap",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: Todura,
+        source_code_link: "https://github.com/Subhajit-Nandi/TODURA",
+    },
+    {
+        name: "CyberViON",
+        description:
+            "CyberViON is a tech blog website built using bootstrap.The blog's primary focus usually centers on the technologies you use most in your daily life. ",
+        tags: [
+            {
+                name: "Html",
+                color: "blue-text-gradient",
+            },
+            {
+                name: "css",
+                color: "green-text-gradient",
+            },
+            {
+                name: "Bootstrap",
+                color: "pink-text-gradient",
+            },
+        ],
+        image: CyberViON,
+        source_code_link: "https://subhajit-nandi.github.io/CyberViON/",
+    },
+];
+
+export { services, technologies, experiences, testimonials, projects, navLinks };
